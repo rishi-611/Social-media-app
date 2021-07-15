947b02b817771e4f09d78fc394ad42c6af0fda16
rishi dubey <rishi18454@gmail.com>
2021-07-15 10:11:17 +0530

    ready for first deployment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = require("./routes/userRouter");
 const postRouter = require("./routes/postsRouter");
 const profileRouter = require("./routes/profileRouter");
+const path = require("path");
 require("./db/mongoose");
 
 const PORT = process.env.PORT || 5000;
@@ -15,5 +16,15 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/profile", profileRouter);
 // app.use("/api/auth", postRouter);
+
+// serve static assets on production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
