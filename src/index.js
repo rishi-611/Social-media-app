9801e7f7fd29aa6ceb302800c90a328dd7c5a8df
rishi dubey <rishi18454@gmail.com>
2021-07-15 12:18:47 +0530

    production fallback route updated

--- src/index.js
+++ src/index.js
@@ -16,14 +16,15 @@ app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/profile", profileRouter);
 // app.use("/api/auth", postRouter);
-
 // serve static assets on production
 if (process.env.NODE_ENV === "production") {
   // set static folder
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "..", "client", "build", "index.html")
+    );
   });
 }
 
